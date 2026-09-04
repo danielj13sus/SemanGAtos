6ccfa8d994e0ec5451111cf8013765713adde66b
danielj13sus <danieljesus130713@gmail.com>
2026-09-03 22:52:35 -0300

    first commit

---
+++ src/main/java/com/semangatos/semangatos/SemanGAtosApplication.java
@@ -0,0 +1,13 @@
+package com.semangatos.semangatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanGAtosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemanGAtosApplication.class, args);
+    }
+
+}
