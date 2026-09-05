3edd01a2423905680f84c2dbdcf2929d876b2020
danielj13sus <danieljesus130713@gmail.com>
2026-09-04 22:16:22 -0300

    Adding the User class

--- src/main/java/com/semangatos/application/SemanGAtosApplication.java
+++ src/main/java/com/semangatos/application/SemanGAtosApplication.java
@@ -1,13 +1,20 @@
-package com.semangatos.semangatos;
+package com.semangatos.application;
 
+import com.semangatos.domains.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SemanGAtosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SemanGAtosApplication.class, args);
+
+        Scanner input = new Scanner(System.in);
+
+        input.close();
     }
 
 }
--- src/test/java/com/semangatos/SemanGAtosApplicationTests.java
+++ src/test/java/com/semangatos/SemanGAtosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.semangatos.semangatos;
+package com.semangatos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
